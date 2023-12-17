63924ae478644492c57a2de83b6897d0c3915a60
Will Pennell <willpennell@protonmail.com>
2023-12-17 23:36:42 +0000

    Very basic ui

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/willpennell/packing-project/router"
@@ -10,6 +12,10 @@ import (
 func main() {
 	dbService := service.NewDBService()
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/**")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 	router.InitializeRoutes(r, dbService)
 	r.Run()
 
