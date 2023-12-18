563618112ebd31bf8e7b923e06b27d4105c21643
Will Pennell <willpennell@protonmail.com>
2023-12-18 08:53:57 +0000

    fixed " from :

--- model/PackedItemsResponse.go
+++ model/PackedItemsResponse.go
@@ -1,7 +1,7 @@
 package model
 
 type PackedItemsInfo struct {
-	ExtraItems int                 `json:"extra_items" binding:"required:`
+	ExtraItems int                 `json:"extra_items" binding:"required"`
 	TotalItems int                 `json:"total_items" binding:"required"`
 	TotalPacks int                 `json:"total_packs" binding:"required"`
 	Packs      map[string]PackInfo `json:"packs" binding:"required"`
